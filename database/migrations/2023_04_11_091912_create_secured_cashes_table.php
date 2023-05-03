bf73e55baf7069342a77d8eaf5bfddc8176ba56b
dammydev2 <damilola.yakubu@yahoo.com>
2023-05-03 14:03:37 +0100

    nullable secured cash

--- database/migrations/2023_04_11_091912_create_secured_cashes_table.php
+++ database/migrations/2023_04_11_091912_create_secured_cashes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(User::class)->constrained();
             $table->double('balance', 8, 2)->default(0);
-            $table->string('pin');
+            $table->string('pin')->nullable();
             $table->timestamps();
         });
     }
